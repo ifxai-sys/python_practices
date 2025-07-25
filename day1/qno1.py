4be01f2da299dbe11971885e09483967e98a02d2
ifxai-sys <f2023266772@umt.edu.pk>
2025-07-25 20:11:53 +0500

    Initial commit

---
+++ day1/qno1.py
@@ -0,0 +1,9 @@
+#qno1: printing name age and height with different daatatypes
+name="IMAN FATIMA"
+age=20
+height=1.524 
+print(name)
+print(age)
+print(height)
+# """ phla concept ya h ka agr input lata hain input() ka through lain ga lakin cin ki tarh ya sirf space sa phla wala letter nai kra ga print ya sara print kra ga space ko bi consider kra ga"""
+# """ 2nd: agr hum ak hi print statement ma tino ko output krain ga to ak hi line ma o ga print agr alag print statement use kararin ga to different line pa ho ga "
