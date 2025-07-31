987a1968a418c059f353a186b2ff78b3e101cd2a
Iman Fatima <ifxai921@gmail.com>
2025-07-31 16:14:54 +0500

    Update loop practice.py

--- loop practice.py
+++ loop practice.py
@@ -1,52 +1,52 @@
 # #qno1: printing even numbers between 1 to 100
 
-# for x in range(1,101):
-#     if(x%2==0):
-#         print(x ," ")
+ for x in range(1,101):
+     if(x%2==0):
+         print(x ," ")
  
 # #qno2: sum of first 50 natural numbers using while loop
-# n=1
-# sum=0
-# while n<=50:
-#      sum+=n
-#      n+=1
-# print(sum)
+ n=1
+ sum=0
+ while n<=50:
+      sum+=n
+      n+=1
+ print(sum)
 
 # #qno3: print the table of the number entered by user
-# m=0
-# n=int(input("Enter a number: "))
-# for m in range (11):
-#     print(f"{n} x {m} = {n*m}")
+ m=0
+ n=int(input("Enter a number: "))
+ for m in range (11):
+     print(f"{n} x {m} = {n*m}")
 
 #qno4: find maximum
-# n1= int(input("Enter number 1: "))
-# n2= int(input("Enter number 2: "))
-# n3= int(input("enter number 3: "))
-# max=n1
-# if n2>max:
-#     max=n2
-# if n3>max:
-#     max=n3
-# print(f"{max} is greater")
+ n1= int(input("Enter number 1: "))
+ n2= int(input("Enter number 2: "))
+ n3= int(input("enter number 3: "))
+ max=n1
+ if n2>max:
+     max=n2
+ if n3>max:
+     max=n3
+ print(f"{max} is greater")
 
 #qno6: printing the fabonacci series:
-# num= int(input("enter a number: "))
-# n1=0
-# n2=1
-# for x in range(num):
-#     print(n1)
-#     n1,n2=n2,n1+n2
+ num= int(input("enter a number: "))
+ n1=0
+ n2=1
+ for x in range(num):
+     print(n1)
+     n1,n2=n2,n1+n2
 
 #qno7:counting the number of digits in number entered by user
-# n= int(input("enter number: "))
-# count=0
-# if n==0:
-#     count=1
-# else:
-#     while (n!=0):
-#         n = n // 10
-#         count+=1
-# print(count)
+ n= int(input("enter number: "))
+ count=0
+ if n==0:
+     count=1
+ else:
+     while (n!=0):
+         n = n // 10
+         count+=1
+ print(count)
 #qno8 : pattern printing
 for i in range(1,5):
     for j in range(i):
@@ -73,22 +73,22 @@ for i in range(4,0,-1):
         print(i, end="")
     print()
 #qno13:finding factorial 
-# fact=1
-# n=int(input("Enter a number:"))
-# if n==0 or n==1:
-#   fact=1
-# else:
-#     for i in range(n,1,-1):
-#         fact=fact*i
-# print(fact)
+ fact=1
+ n=int(input("Enter a number:"))
+ if n==0 or n==1:
+   fact=1
+ else:
+     for i in range(n,1,-1):
+         fact=fact*i
+ print(fact)
 #qno14: reversing a number using loops 
-# n=int(input("Enter a number:"))
-# r_n=0
-# while n>0:
-#      digit=n%10
-#      r_n=r_n*10+digit
-#      n=n // 10
-# print("reversed number: ", r_n)
+ n=int(input("Enter a number:"))
+ r_n=0
+ while n>0:
+      digit=n%10
+      r_n=r_n*10+digit
+      n=n // 10
+ print("reversed number: ", r_n)
 #qno15:prime numbers between 1 and 50
 import math
 for i in range(2,50):
